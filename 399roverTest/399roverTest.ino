1f5f13b03a4ac84185f69a3d78e9776473c59c22
Travis Mundy <mundyt4@mymacewan.ca>
2022-12-05 21:35:37 -0700

    My final build seems to work

--- 399roverTest/399roverTest.ino
+++ 399roverTest/399roverTest.ino
@@ -1,4 +1,8 @@
 #include "Adafruit_VL53L0X.h"
+#include <Wire.h>
+#include "Adafruit_VEML7700.h"
+
+Adafruit_VEML7700 veml = Adafruit_VEML7700();
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
@@ -9,16 +13,48 @@ void setup() {
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(9, OUTPUT);
+  pinMode(7, OUTPUT);
 
   // wait until serial port opens for native USB devices
   while (! Serial) {
-    delay(1);
+    delay(10);
   }
   
   if (!lox.begin()) {
     Serial.println(F("Failed to boot VL53L0X"));
-    while(1);
+    //while(1);
+  } else {
+    Serial.println("Found a VL53L0X sensor");
   }
+
+  if (!veml.begin()) {
+    Serial.println("Sensor not found");
+    while (1);
+  }else{
+    Serial.println("Sensor found");
+  }
+
+  Serial.print(F("Gain: "));
+  switch (veml.getGain()) {
+    case VEML7700_GAIN_1: Serial.println("1"); break;
+    case VEML7700_GAIN_2: Serial.println("2"); break;
+    case VEML7700_GAIN_1_4: Serial.println("1/4"); break;
+    case VEML7700_GAIN_1_8: Serial.println("1/8"); break;
+  }
+
+  Serial.print(F("Integration Time (ms): "));
+  switch (veml.getIntegrationTime()) {
+    case VEML7700_IT_25MS: Serial.println("25"); break;
+    case VEML7700_IT_50MS: Serial.println("50"); break;
+    case VEML7700_IT_100MS: Serial.println("100"); break;
+    case VEML7700_IT_200MS: Serial.println("200"); break;
+    case VEML7700_IT_400MS: Serial.println("400"); break;
+    case VEML7700_IT_800MS: Serial.println("800"); break;
+  }
+
+  veml.setLowThreshold(10000);
+  veml.setHighThreshold(20000);
+  veml.interruptEnable(true);
 
 }
 
@@ -26,34 +62,57 @@ void loop() {
   uint16_t distance = checkDistance();
   Serial.print("passed distance: ");
   Serial.println(distance);
+  delay(150);
+  
+  uint16_t reading = veml.readLux();
+  Serial.print("Reading: ");
+  Serial.println(reading);
+  buzzerDynamic(reading);
 
-  if(distance < 80){
+  if(distance < 350){
     coast();
-    delay(2000);
-    turnLeft(150);
-    delay(450);
+    delay(1000);
+    turnLeft(255);
+    delay(900);
     coast();
+    uint16_t reading = veml.readLux();
+    Serial.print("Reading: ");
+    Serial.println(reading);
+    buzzerDynamic(reading);
     delay(2000);
 
 
     distance = checkDistance();
-    if(distance < 80){
-      turnRight(150);
-      delay(900);
+    if(distance < 200){
+      turnRight(255);
+      delay(1800);
       coast();
+      uint16_t reading = veml.readLux();
+      Serial.print("Reading: ");
+      Serial.println(reading);
+      buzzerDynamic(reading);
       delay(2000);
 
-      if(distance < 80){
-        turnRight(150);
-        delay(450);
+      if(distance < 200){
+        turnRight(255);
+        delay(900);
         coast();
+        uint16_t reading = veml.readLux();
+        Serial.print("Reading: ");
+        Serial.println(reading);
+        buzzerDynamic(reading);
         delay(2000);
       }
     }
   } else{
-    moveForwards(130);
+    moveForwards(255);
+    //motorLeftReverse(200);
   }
-  moveForwards(130);
+    //moveForwards(255);
+    //motorLeftReverse(200);
+   //motorLeftForward(150);
+  //motorRightForward(200);
+  //motorRightReverse(200);
 }
 
 uint16_t checkDistance(){
@@ -63,8 +122,6 @@ uint16_t checkDistance(){
   lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
   uint16_t distance = measure.RangeMilliMeter;
-
-  delay(100);
     
   return distance;
 }
@@ -120,5 +177,19 @@ void moveBackwards(int speed){
 
 void moveForwards(int speed){
   motorLeftForward(speed);
-  motorRightForward(speed);
+  motorRightForward(speed + 50);
+}
+
+
+void buzzerDynamic(uint16_t reading){
+  reading = reading;
+  int toneValue = 1400 * reading + 400;
+  int delayValue = 0.13896609227 * reading + 49.8610339077;
+  tone(7, toneValue);
+  delay(delayValue);
+  noTone(7);
+  delay(delayValue);
+  tone(7, toneValue);
+  delay(delayValue);
+  noTone(7);
 }
